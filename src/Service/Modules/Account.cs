c9f275e9498cd4b3a3c5fb9223499f13909d1dab
Soren Z.Vand <sunrise.freedom@gmail.com>
2015-01-08 12:16:05 +0330

    - change project address

---
+++ src/Service/Modules/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy;
+
+namespace Service.Modules
+{
+    public class Account : NancyModule
+    {
+        public Account() : base("account")
+        {
+            Get["/get"] = _ => "Hello Account.";
+        }
+    }
+}
